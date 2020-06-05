b73051bc7e3f61b046a0d7604b727eec19f75ab8
david lin <davidlindev@gmail.com>
2020-06-05 09:42:57 +0100

    add

--- zip_dataset.py
+++ zip_dataset.py
@@ -1,11 +1,12 @@
+# coding=utf-8
 # 将一个目录下的所有文件和文件夹按照原来的文件结构打包，每个包不超过10g
 
 import zipfile
 import os
 from tqdm import tqdm
 
-dataset_dir = "/home/lin/Desktop/github/mini/jinzhangpeng_shop_mini/"
-zip_dir = "/home/lin/Desktop/zip"
+dataset_dir = "/media/1tDisk/data/processing/siim"
+zip_dir = "/media/1tDisk/data/processing/siim_zip"
 dataset_name = "siim"
 
 zip_num = 1
@@ -15,13 +16,12 @@ f = zipfile.ZipFile(curr_zip_path, "a", zipfile.ZIP_DEFLATED)
 
 files_list = []
 list_size = 0
-zip_tot_size = 9.7 * 1024 * 128
+zip_tot_size = 9.7 * 1024 * 1024 * 50
 zip_left_size = zip_tot_size
 # 9.7 * 1024 * 1024 * 1024
 
 
 for dirpath, dirnames, filenames in tqdm(os.walk(os.path.join(dataset_dir))):
-    print(dirpath, filenames)
     for filename in filenames:
         files_list.append(
             [
@@ -31,10 +31,12 @@ for dirpath, dirnames, filenames in tqdm(os.walk(os.path.join(dataset_dir))):
         )
         list_size += os.path.getsize(os.path.join(dirpath, filename))
         if list_size >= zip_left_size * 1.1:  # 如果当前列表中未压缩文件的大小大于 1.1 倍zip包能装的大小
+            print("Writting {} file".format(len(files_list)))
             # 将列表里所有的文件写入zip
             for pair in files_list:
                 f.write(pair[0], pair[1])
             curr_size = os.path.getsize(curr_zip_path)
+            print("curr size is: {} M".format(curr_size/1024**2) )
             files_list = []
             if curr_size >= zip_tot_size:
                 f.close()
@@ -42,6 +44,7 @@ for dirpath, dirnames, filenames in tqdm(os.walk(os.path.join(dataset_dir))):
                 curr_name = "{}-{}.zip".format(dataset_name, zip_num)
                 curr_zip_path = os.path.join(zip_dir, curr_name)
                 f = zipfile.ZipFile(curr_zip_path, "a", zipfile.ZIP_DEFLATED)
+                print("+++ opening new zip: {}  +++".format( curr_name) )
             else:
                 zip_left_space = zip_tot_size - curr_size
 
