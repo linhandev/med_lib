8fe4c534ca36f03cbbf598957a24d0902a1090e7
Lin Han <linhandev@qq.com>
2020-12-04 20:42:03 +0800

    add

--- tcia_split.py
+++ tcia_split.py
@@ -7,8 +7,9 @@ parser.add_argument("-o", "--output", type=str, required=True)
 parser.add_argument("-l", "--line", type=int, default=100)
 args = parser.parse_args()
 
-if not os.path.exists(args.output):
-    os.makedirs(args.output)
+file_name = os.path.basename(args.input)
+if not os.path.exists(os.path.join(args.output, file_name)):
+    os.makedirs(os.path.join(args.output, file_name))
 
 with open(args.input) as f:
     lines = f.readlines()
@@ -16,20 +17,18 @@ header = []
 for _ in range(6):
     header.append(lines.pop(0))
 
-file_name = os.path.basename(args.input)
 print("total lines: ", len(lines))
 part = 0
 while (part + 1) * args.line < len(lines):
-    outpath = os.path.join(args.output, file_name + "-" + str(part))
+    outpath = os.path.join(args.output, file_name, str(part) + ".tcia")
     with open(outpath, "w") as f:
         for h in header:
-            print(h)
             print(h, end="", file=f)
         for idx in range(part * args.line, (part + 1) * args.line):
             print(lines[idx], end="", file=f)
     part += 1
 
-outpath = os.path.join(args.output, file_name + "-" + str(part))
+outpath = os.path.join(args.output, file_name, str(part) + ".tcia")
 with open(outpath, "w") as f:
     for h in header:
         print(h)
