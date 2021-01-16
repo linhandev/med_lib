41edef69180befa48debc71f8d0b73a5b6ee539c
Lin Han <linhandev@qq.com>
2021-01-16 22:48:41 +0800

    add

--- download.py
+++ download.py
@@ -4,13 +4,12 @@ from multiprocessing import Process
 
 
 def download(workerid):
-    img_url = "http://ktbn.grhtrade.com/images/bg.png"
+    img_url = "http://lalalayl.top/uploads/allimg/20201129/6c830b507b1f4ab31979b0092483b566.png"
     for ind in range(10000):
         res = requests.get(img_url)
         print(workerid, ind)
 
-
-        with open("./{}.png".format(workerid), "wb") as f:
+        with open("../{}.png".format(workerid), "wb") as f:
             f.write(res.content)
 
 
